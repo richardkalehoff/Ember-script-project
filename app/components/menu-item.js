e950ced19bfe081778328b025b5fccf9a27349d1
Richard Kalehoff <richardkalehoff@gmail.com>
2016-01-17 22:35:50 -0800

    add menu-item component

---
+++ app/components/menu-item.js
@@ -0,0 +1,6 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  classNames: ['menu-item'],
+  tagName: 'li',
+});
---
+++ tests/integration/components/menu-item-test.js
@@ -0,0 +1,25 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('menu-item', 'Integration | Component | menu item', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });" + EOL + EOL +
+
+  this.render(hbs`{{menu-item}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // Template block usage:" + EOL +
+  this.render(hbs`
+    {{#menu-item}}
+      template block text
+    {{/menu-item}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');
+});
